0e5e1e7c7aef6c33dedfe692237f21ef01cd0962
Molly Moran <40001570+mgmoran@users.noreply.github.com>
2019-03-29 13:50:04 -0400

    Extracting the data to be annotated (ugly version)

---
+++ DataExtractor.py
@@ -0,0 +1,30 @@
+##This is an ugly first pass just to get all the information we need for our annotators...
+##We can organize the data better later, maybe with lots of tuples, associating sentences with
+##documents, with their instances, etc...but you can use this for now to see all the sentences
+## organized by document that have a PropBank annotated verb in them. (often more than one 
+##verb (instance) per sentence)
+
+##Stats: 
+#9000 "instances" (individual verbs annotated with their arguments)
+#186 total documents represented among all the instances
+
+
+import nltk
+from nltk.corpus import treebank,propbank
+
+# Grabbing the first 9000 propbank instances (these 9000 instances map to 186 documents total)
+instances = propbank.instances()[:9000]
+#Getting a set of documents represented by these 9000 instances
+docs = set(list([instance.fileid for instance in instances]))
+#Filling a dictionary of WSJ documents (keys) mapped to all the propbank instances contained in each doc (values)
+instancedict = {}
+for doc in docs:
+	instancedict[doc] = [instance for instance in instances if instance.fileid==doc]
+
+#Create a dictionary of each sentence in the document corresponding to an instance, for each doc
+#docs are keys, values are lists of sentences
+docdict = {}
+for doc in instancedict:
+	sentlist = [instance.sentnum for instance in instancedict[doc]]
+	docdict[doc] = set([treebank.sents(doc)[sentid] for sentid in sentlist])
+
