27ef9bbc7d56c51416ea925cbb7c6c48a9ee1b67
Molly <mollymoran@brandeis.edu>
2019-04-10 20:33:11 -0400

    Final Data Extractor Version

--- DataExtractor.py
+++ DataExtractor.py
@@ -1,8 +1,7 @@
-##This is an ugly first pass just to get all the information we need for our annotators...
-##We can organize the data better later, maybe with lots of tuples, associating sentences with
-##documents, with their instances, etc...but you can use this for now to see all the sentences
-## organized by document that have a PropBank annotated verb in them. (often more than one 
-##verb (instance) per sentence)
+##Extracts annotatable sentences that correspond to Instance objects in Propbank.
+##Eventually we might want to organize this data better, perhaps into tuples associating
+##sentences with their doc name, instance number, etc.
+
 
 ##Stats: 
 #9000 "instances" (individual verbs annotated with their arguments)
@@ -16,16 +15,26 @@ from collections import defaultdict
 
 # Grabbing the first 9000 propbank instances (these 9000 instances map to 186 documents total)
 instances = propbank.instances()[:9000]
-#Getting a set of documents represented by these 9000 instances
+#Extracting the set of documents represented by these 9000 instances
 docs = set(list([instance.fileid for instance in instances]))
-#Filling a dictionary of WSJ documents (keys) mapped to all the propbank instances contained in each doc (values)
-instancedict = {}
+#A dictionary that maps WSJ documents (keys) to all propbank instances contained in each doc (values)
+instancedict = defaultdict(list)
 for doc in docs:
-    instancedict[doc] = [instance for instance in instances if instance.fileid==doc]
+    instancedict[doc] = [instance for instance in instances if instance.fileid == doc]
 
-#Create a dictionary of each sentence in the document corresponding to an instance, for each doc
-#docs are keys, values are lists of sentences. Sentences are tokenized.
-docdict = defaultdict(list)
+#A dictionary of WSJ docs mapped to every sentence in that doc associated to a PB instance.
+#Each sentence is then mapped to a list of its PB instances.
+sentdict = defaultdict(lambda: defaultdict(list))
 for doc in instancedict:
+        sentlist = set([instance.sentnum for instance in instancedict[doc]])
+        for num in sentlist:
+            sentdict[doc][num] = [instance for instance in instancedict[doc] if instance.sentnum==num]
+
+#For simplicity's sake, I filter out all sentences that have more than one
+#PB instance.
+#Creates a dictionary of WSJ doc names mapped to viable sentences for annotation in that doc.
+#Sentences are tokenized.
+docdict = defaultdict(list)
+for doc in sentdict:
     sentlist = set([instance.sentnum for instance in instancedict[doc]])
-    docdict[doc] = [treebank.sents(doc)[sentid] for sentid in sentlist]
+    docdict[doc] = [treebank.sents(doc)[sentid] for sentid in sentdict[doc] if len(sentdict[doc][sentid])==1]
