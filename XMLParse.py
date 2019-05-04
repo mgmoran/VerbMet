92cc5ccb7a4fc6c45d5e333dcc0cc28f8366700b
Molly <mollymoran@brandeis.edu>
2019-05-04 17:40:05 -0400

    Alpha IAA Score added to XMLParse.py

--- XMLParse.py
+++ XMLParse.py
@@ -1,11 +1,14 @@
 import sys
 import lxml.html
 from lxml import etree
+import nltk
 import csv
+from nltk import agreement
+from nltk.metrics.agreement import AnnotationTask
+from nltk.metrics import interval_distance, binary_distance, masi_distance
 
 def XMLParse(a1,a2,a3):
         exceptions = ["am", "are", "is", "were", "was", "been", "be", "being", "has", "have", "had", "do", "done", "compared", "aware","used","said", "overlap", "noted","think", "Performing","go","seems","uses","says","raising","appear","received","boost","reciting","reprove","responded","leave"]
-
         ### Annotator 1
         annotator1 = etree.parse(a1)
         ### all tags
@@ -78,15 +81,31 @@ def XMLParse(a1,a2,a3):
         ##Row 1: Type: Literal or Nonliteral
         ##Row 2: Mismatched argument, if one exists
         verbentities = zip(shared1, shared2, shared3)
+        tupls = []
+        index = 0
         with open('IAACalculations.csv', 'w') as f:
             writer = csv.writer(f, delimiter=",")
             writer.writerow(("Verb","Annotator 1","Annotator 2","Annotator 3"))
             for e in verbentities:
-                writer.writerow( (e[0].attrib['text'],e[0].attrib['type'],e[1].attrib['type'],e[2].attrib['type']))
-                mismatch1 = [x.attrib['fromText'] for x in mismatches1 if x.attrib['toID']== e[0].attrib['id']]
-                mismatch2 = [x.attrib['fromText'] for x in mismatches2 if x.attrib['toID'] == e[1].attrib['id']]
-                mismatch3 = [x.attrib['fromText'] for x in mismatches3 if x.attrib['toID'] == e[2].attrib['id']]
-                writer.writerow( (e[0].attrib['text'], mismatch1, mismatch2, mismatch3 ))
+                verb = e[0].attrib['text']
+                type1 = e[0].attrib['type']
+                type2 = e[1].attrib['type']
+                type3 = e[2].attrib['type']
+                tupls.extend( [ ('a1', frozenset([type1]), frozenset([verb]) ),  ('a2', frozenset([type2]), frozenset([verb]) ), ('a3', frozenset([type3]), frozenset([verb]) )])
+                writer.writerow( (e[0].attrib['text'],e[0].attrib['type'],e[1].attrib['type'],e[2].attrib['type'] ) )
+                if type1=='Nonliteral' and type2=='Nonliteral'and type3=='Nonliteral':
+                    mismatch1 = [x.attrib['fromText'] for x in mismatches1 if x.attrib['toID']== e[0].attrib['id']]
+                    mismatch2 = [x.attrib['fromText'] for x in mismatches2 if x.attrib['toID'] == e[1].attrib['id']]
+                    mismatch3 = [x.attrib['fromText'] for x in mismatches3 if x.attrib['toID'] == e[2].attrib['id']]
+                    tupls.extend([('a1', frozenset(mismatch1), frozenset([index])),('a2', frozenset(mismatch2), frozenset([index])),('a3', frozenset(mismatch3), frozenset([index]))])
+                    index +=1
+                    writer.writerow( (e[0].attrib['text'], mismatch1, mismatch2, mismatch3))
+        f.close()
+        ### Calculating IAA
+        t = agreement.AnnotationTask(data=tupls,distance=binary_distance)
+        print("Alpha: %.2f" % (t.alpha()))
+
+
 
 if __name__ == "__main__":
     XMLParse('Annotator1Final.xml','Annotator2Final.xml','Annotator3Final.xml')
